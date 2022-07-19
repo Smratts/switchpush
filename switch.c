bc79a61a8b88765b15343782f7a14d7ffa6a2223
error <mail@gamil.com>
2022-07-19 16:18:05 +0530

    switc.c commit

---
+++ switch.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define y 2
+#define z 23
+int main()
+{
+int x=2;
+switch(x)
+{case y: printf("number is 2");
+break;
+default : printf("default");
+break;
+case z: printf("numberis 23");
+break;
+}
+}
